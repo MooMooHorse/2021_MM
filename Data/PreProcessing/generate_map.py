5c5cca69dfbd5db5cb19362380671119202352b8
MooMooHorse <haor.20@intl.zju.edu.cn>
2022-02-10 11:22:01 +0800

    Try map

--- Data/PreProcessing/generate_map.py
+++ Data/PreProcessing/generate_map.py
@@ -1,3 +1,8 @@
+"""
+python -m pip install basemap-data
+python -m pip install basemap-data-hires
+https://github.com/matplotlib/basemap
+"""
 def plot_map(lat,lon):
     from mpl_toolkits.basemap import Basemap
     import matplotlib.pyplot as plt
@@ -9,13 +14,12 @@ def plot_map(lat,lon):
     m.drawstates()
     m.fillcontinents(color='#04BAE3', lake_color='#FFFFFF')
     m.drawmapboundary(fill_color='#FFFFFF')
-
     
     x,y = m(lon,lat)
     m.plot(x,y,'ro',markersize=4,alpha=.5)
 
-    # x,y = m(lon2,lat2)
-    # m.plot(x,y,'go',markersize=4,alpha=.5)
+    # # x,y = m(lon2,lat2)
+    # # m.plot(x,y,'go',markersize=4,alpha=.5)
 
     plt.title('Geo Plotting')
     plt.show()

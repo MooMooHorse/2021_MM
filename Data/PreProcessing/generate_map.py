db8ef0bf28f6ba6512b5fb8db772426ed2b15093
MooMooHorse <haor.20@intl.zju.edu.cn>
2022-02-10 10:09:49 +0800

    Finish Plotting

--- Data/PreProcessing/generate_map.py
+++ Data/PreProcessing/generate_map.py
@@ -1,27 +1,23 @@
-from mpl_toolkits.basemap import Basemap
-import matplotlib.pyplot as plt
+def plot_map(lat,lon):
+    from mpl_toolkits.basemap import Basemap
+    import matplotlib.pyplot as plt
 
-m = Basemap(projection='mill',llcrnrlat=-60,urcrnrlat=90,\
-                llcrnrlon=-180,urcrnrlon=180,resolution='l')
-m.drawcoastlines()
-m.drawcountries()
-m.drawstates()
-m.fillcontinents(color='#04BAE3', lake_color='#FFFFFF')
-m.drawmapboundary(fill_color='#FFFFFF')
+    m = Basemap(llcrnrlon=140.874088,llcrnrlat=-39.054526,urcrnrlon=151.567450,urcrnrlat=-33.122107,projection='mill',\
+                    resolution='l')
+    m.drawcoastlines()
+    m.drawcountries()
+    m.drawstates()
+    m.fillcontinents(color='#04BAE3', lake_color='#FFFFFF')
+    m.drawmapboundary(fill_color='#FFFFFF')
 
-lat = -35
-lon = 149
+    
+    x,y = m(lon,lat)
+    m.plot(x,y,'ro',markersize=4,alpha=.5)
 
+    # x,y = m(lon2,lat2)
+    # m.plot(x,y,'go',markersize=4,alpha=.5)
 
-lat2 = 40,33,44,31,30
-lon2 = -113,-100,-102,-111,-112
+    plt.title('Geo Plotting')
+    plt.show()
 
-x,y = m(lon,lat)
-m.plot(x,y,'ro',markersize=4,alpha=.5)
-
-x,y = m(lon2,lat2)
-m.plot(x,y,'go',markersize=4,alpha=.5)
-
-plt.title('Geo Plotting')
-plt.show()
-# plt.savefig("map1.png")
+plot_map([-37],[146])

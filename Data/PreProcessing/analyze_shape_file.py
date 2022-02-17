1703660f1e48ede6480f49d5a75af04fee80cc74
MooMooHorse <haor.20@intl.zju.edu.cn>
2022-02-17 20:58:05 +0800

    Change Project To 2022

--- Data/PreProcessing/analyze_shape_file.py
+++
@@ -1,4 +0,0 @@
-import geopandas
-path_to_data = geopandas.datasets.get_path("STE_2021_AUST_GDA2020.shp")
-gdf = geopandas.read_file(path_to_data)
-print(gdf)
--- Data/PreProcessing/bound.m
+++
@@ -1,3 +0,0 @@
-China=shaperead('STE_2021_AUST_GDA2020.shp','UseGeoCoords',true); %读入中国的国界文件shp文件
-Scale=100; 
-[Z,R]=vec2mtx(lat,lon,Scale,'filled');
